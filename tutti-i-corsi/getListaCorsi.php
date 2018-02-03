2a32cd300c80d8db660d8da907eb46afecd8c49d
Tommaso Azzalin <azzalintommaso@gmail.com>
2018-02-03 13:19:57 +0100

    tolta "Altre attività" da lista tutti i corsi

--- tutti-i-corsi/getListaCorsi.php
+++ tutti-i-corsi/getListaCorsi.php
@@ -6,7 +6,7 @@
         $db=Session::get("db");
         $giorno=GlobalVar::getPost("giorno");
         $ora=GlobalVar::getPost("ora");
-        $q="SELECT Nome,Aula,Durata,MaxPosti AS PostiTotali,PostiRimasti FROM Corsi C INNER JOIN SessioniCorsi S ON C.ID_Corso=S.ID_Corso WHERE Giorno='".$giorno."' AND Ora='".$ora."' ORDER BY Nome ASC";
+        $q="SELECT Nome,Aula,Durata,MaxPosti AS PostiTotali,PostiRimasti FROM Corsi C INNER JOIN SessioniCorsi S ON C.ID_Corso=S.ID_Corso WHERE Giorno='".$giorno."' AND Ora='".$ora."' AND Nome != 'Altre attività' ORDER BY Nome ASC";
         $res=$db->qikQuery($q); //ritornato un array
         $jsonData=json_encode($res);
         echo $jsonData;
