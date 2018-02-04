a35e1e87ffecada438d0ceb6dacc75bcb2d5159f
Tommaso Azzalin <azzalintommaso@gmail.com>
2018-02-04 14:43:27 +0100

    fix a amministrazione - pannello E

--- amministrazione/getSessioniCorso.php
+++ amministrazione/getSessioniCorso.php
@@ -6,7 +6,7 @@ require_once "../classes.php";
         $db=Session::get("db");
         $nomeC=GlobalVar::getPost("nomeCorso");
 
-        $query="SELECT ID_Corso AS id,Durata AS d,Aula AS a,MaxPosti AS pt FROM Corsi WHERE Nome='".$nomeC."'";
+        $query='SELECT ID_Corso AS id,Durata AS d,Aula AS a,MaxPosti AS pt FROM Corsi WHERE Nome="'.$nomeC.'"';
         $datiCorso=$db->qikQuery($query); //ritornato un array
         
         $query2="SELECT Giorno AS g,Ora AS o, PostiRimasti AS pr, ID_SessioneCorso AS id_sc FROM SessioniCorsi WHERE ID_Corso=".intval($datiCorso[0]["id"])." ORDER BY Giorno,Ora";
