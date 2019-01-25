e20d13dd3a1a1bd2a70402065a93279333d0752d
Tommaso Azzalin <azzalintommaso@gmail.com>
2019-01-25 19:24:57 +0100

    sistemato resetCorsiStudente.php in i-miei-corsi

--- i-miei-corsi/script/resetCorsiStudente.php
+++ i-miei-corsi/script/resetCorsiStudente.php
@@ -2,82 +2,38 @@
 require_once "../../caricaClassi.php";
 require_once "../../connettiAlDB.php";
 require_once "../../funzioni.php";
+
+if(GlobalVar::SERVER("REQUEST_METHOD") !== "POST") header("Location: ../../");
+
 Session::open();
-$utente=Session::get("utente");
+$utente = Session::get("utente");
+$ID_Persona = $utente->getId();
+//SCRIPT DI RESET TOTALE DI UNO STUDENTE
 
-if(GlobalVar::SERVER("REQUEST_METHOD")==="POST") {
-    /**************************************************************/
-    /*  SCRIPT DI RESET TOTALE DELLE ISCRIZIONI DI UNO STUDENTE   */
-    /**************************************************************/
+// PASSAGGIO 1: recupero gli ID delle sessioni dei corsi a cui si è iscritto
+$corsiStudente = getCorsiStudente($db, $ID_Persona);
+if($corsiStudente === "errore_db_corsi_iscritti_studente") echo $corsiStudente;
+else {
+    $ID_SessioneCorso = [];
+    for ($i = 0, $l = sizeof($corsiStudente); $i < $l; $i++) array_push($ID_SessioneCorso, $corsiStudente[$i]["ID_SessioneCorso"]);
 
-    $ID_Persona=$utente->getId();
+    unset($corsiStudente);
 
-    // VARIABILI DI CONTROLLO
-    $tuttoOk=true;
-    $controlloQuery=array();
+    // PASSAGGIO 2: recupero gli ID delle iscrizioni dello studente
+    $ID_Iscrizione = getIscrizioniStudente($db, $ID_Persona, $ID_SessioneCorso);
+    if ($ID_Iscrizione === "errore_iscrizioni_sessioni") echo $ID_Iscrizione;
 
-    //----RECUPERO DELLE SESSIONI DEI CORSI A CUI SI E' ISCRITTO L'UTENTE
-    $qID_SessioneCorso="SELECT ID_SessioneCorso FROM Iscrizioni WHERE ID_Studente=$ID_Persona";
-    $rID_SessioneCorso=$db->queryDB($qID_SessioneCorso);
-    $controlloQuery[0]=$db->checkQuery();
+    // PASSAGGIO 3: rimozione delle istanze nel Registro Presenze delle iscrizioni con codice iscrizione presente nell'array $ID_Iscrizione
+    elseif (!rimuoviIstanzeRegistro($db, $ID_Iscrizione)) echo "errore_rimozione_istanze_registro";
 
-    //----RECUPERO I CODICI DELLE ISCRIZIONI REGISTRATE SULLE PRECEDENTI SESSIONI DEI CORSI
-    $qID_Iscrizione="SELECT ID_Iscrizione FROM Iscrizioni WHERE ID_SessioneCorso IN (";
-    for($i=0,$l=sizeof($rID_SessioneCorso);$i<$l;$i++) {
-        $qID_Iscrizione.=$rID_SessioneCorso[$i]["ID_SessioneCorso"];
-        if($i != ($l-1)) $qID_Iscrizione.=", ";
-    }
-    $qID_Iscrizione.=") AND ID_Studente=".$ID_Persona;
-    $rID_Iscrizione=$db->queryDB($qID_Iscrizione);
-    $controlloQuery[1]=$db->checkQuery();
+    // PASSAGGIO 4: rimozione delle iscrizioni con codice sessione corso presente nell'array $ID_SessioneCorso
+    elseif (!rimuoviIscrizioni($db, $ID_Persona, $ID_SessioneCorso)) echo "errore_rimozione_iscrizioni";
 
-    //----ELIMINAZIONE DELLE ISTANZE NEL REGISTRO DELLE PRESENZE RELATIVE AI CODICI DELLE ISCRIZIONI PRECEDENTI
-    $qDelRegPresenze="DELETE FROM RegPresenze WHERE ID_Iscrizione IN (";
-    for($i=0,$l=sizeof($rID_Iscrizione);$i<$l;$i++) {
-        $qDelRegPresenze.=$rID_Iscrizione[$i]["ID_Iscrizione"];
-        if($i != ($l-1)) $qDelRegPresenze.=", ";
-    }
-    $qDelRegPresenze.=")";
-    $rDelRegPresenze=$db->queryDB($qDelRegPresenze);
-    $controlloQuery[2]=$db->checkQuery();
+    // PASSAGGIO 5: aggiornamento dei posti disponibili nelle sessioni dei corsi
+    elseif (!aggiornaSessioniCorsi($db, $ID_SessioneCorso)) echo "errore_aggiornamento_sessionicorsi";
 
-    //----ELIMINAZIONE DEI CODICI DELLE ISCRIZIONI REGISTRATE SULLE PRECEDENTI SESSIONI DEI CORSI
-    $qDelIscrizioni="DELETE FROM Iscrizioni WHERE ID_SessioneCorso IN (";
-    for($i=0,$l=sizeof($rID_SessioneCorso);$i<$l;$i++) {
-        $qDelIscrizioni.=$rID_SessioneCorso[$i]["ID_SessioneCorso"];
-        if($i != ($l-1)) $qDelIscrizioni.=", ";
-    }
-    $qDelIscrizioni.=") AND ID_Studente=".$ID_Persona;
-    $rDelIscrizioni=$db->queryDB($qDelIscrizioni);
-    $controlloQuery[3]=$db->checkQuery();
+    // PASSAGGIO 6: aggiornamento dei dati della persona
+    elseif (!$db->queryDB("UPDATE Persone SET GiornoIscritto=0, OraIscritta=0 WHERE ID_Persona=$ID_Persona")) echo "errore_aggiornamento_persone";
 
-    //----AGGIORNO I POSTI DISPONIBILI IN SessioniCorsi
-    $qUpdSessioniCorsi="UPDATE SessioniCorsi SET PostiRimasti=PostiRimasti+1 WHERE ID_SessioneCorso IN (";
-    for($i=0,$l=sizeof($rID_SessioneCorso);$i<$l;$i++) {
-        $qUpdSessioniCorsi.=$rID_SessioneCorso[$i]["ID_SessioneCorso"];
-        if($i != ($l-1)) $qUpdSessioniCorsi.=", ";
-    }
-    $qUpdSessioniCorsi.=")";
-    $rUpdSessioniCorsi=$db->queryDB($qUpdSessioniCorsi);
-    $controlloQuery[4]=$db->checkQuery();
-
-    //----AGGIORNO I CONTATORI DELLE REGISTRAZIONI DELLL'UTENTE NEL DATABASE
-    $qUpdPersone="UPDATE Persone SET GiornoIscritto=0, OraIscritta=0 WHERE ID_Persona=$ID_Persona";
-    $rUpdPersone=$db->queryDB($qUpdPersone);
-    $controlloQuery[5]=$db->checkQuery();
-
-    //----AGGIORNO L'OGGETTO DELL'UTENTE
-    $utente->setGiornoIscritto(0);
-    $utente->setOraIscritta(0);
-
-
-    //----CONTROLLO CHE TUTTE LE QUERY SIANO ANDATE A BUON FINE
-    $i=0; $l=sizeof($controlloQuery);
-    while($i<$l && $tuttoOk) if($controlloQuery[$i] === false) $tuttoOk=false;
-
-    echo ($tuttoOk) ? "reset-effettuato" : "reset-non-effettuato";
-
-} else {
-    header("Location: ../../");
+    else echo "reset-effettuato";
 }
-?>
