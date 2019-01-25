4882d61dcc3e009c1d347eed9fda11e828625f01
Tommaso Azzalin <azzalintommaso@gmail.com>
2019-01-25 19:29:02 +0100

    sistemato resetCorsiStudente.php in i-miei-corsi

--- i-miei-corsi/script/resetCorsiStudente.php
+++ i-miei-corsi/script/resetCorsiStudente.php
@@ -7,11 +7,11 @@ if(GlobalVar::SERVER("REQUEST_METHOD") !== "POST") header("Location: ../../");
 
 Session::open();
 $utente = Session::get("utente");
-$ID_Persona = $utente->getId();
+
 //SCRIPT DI RESET TOTALE DI UNO STUDENTE
 
 // PASSAGGIO 1: recupero gli ID delle sessioni dei corsi a cui si è iscritto
-$corsiStudente = getCorsiStudente($db, $ID_Persona);
+$corsiStudente = getCorsiStudente($db, $utente->getId());
 if($corsiStudente === "errore_db_corsi_iscritti_studente") echo $corsiStudente;
 else {
     $ID_SessioneCorso = [];
@@ -20,20 +20,24 @@ else {
     unset($corsiStudente);
 
     // PASSAGGIO 2: recupero gli ID delle iscrizioni dello studente
-    $ID_Iscrizione = getIscrizioniStudente($db, $ID_Persona, $ID_SessioneCorso);
+    $ID_Iscrizione = getIscrizioniStudente($db, $utente->getId(), $ID_SessioneCorso);
     if ($ID_Iscrizione === "errore_iscrizioni_sessioni") echo $ID_Iscrizione;
 
     // PASSAGGIO 3: rimozione delle istanze nel Registro Presenze delle iscrizioni con codice iscrizione presente nell'array $ID_Iscrizione
     elseif (!rimuoviIstanzeRegistro($db, $ID_Iscrizione)) echo "errore_rimozione_istanze_registro";
 
     // PASSAGGIO 4: rimozione delle iscrizioni con codice sessione corso presente nell'array $ID_SessioneCorso
-    elseif (!rimuoviIscrizioni($db, $ID_Persona, $ID_SessioneCorso)) echo "errore_rimozione_iscrizioni";
+    elseif (!rimuoviIscrizioni($db, $utente->getId(), $ID_SessioneCorso)) echo "errore_rimozione_iscrizioni";
 
     // PASSAGGIO 5: aggiornamento dei posti disponibili nelle sessioni dei corsi
     elseif (!aggiornaSessioniCorsi($db, $ID_SessioneCorso)) echo "errore_aggiornamento_sessionicorsi";
 
     // PASSAGGIO 6: aggiornamento dei dati della persona
-    elseif (!$db->queryDB("UPDATE Persone SET GiornoIscritto=0, OraIscritta=0 WHERE ID_Persona=$ID_Persona")) echo "errore_aggiornamento_persone";
+    elseif (!$db->queryDB("UPDATE Persone SET GiornoIscritto=0, OraIscritta=0 WHERE ID_Persona=".$utente->getId())) echo "errore_aggiornamento_persone";
 
-    else echo "reset-effettuato";
+    else {
+        $utente->setGiornoIscritto(0);
+        $utente->setOraIscritta(0);
+        echo "reset-effettuato";
+    }
 }
